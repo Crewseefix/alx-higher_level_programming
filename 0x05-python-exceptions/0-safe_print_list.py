961ce88bdfdc06ed9d323212fb641a16b4280cce
crewseefix <tostirma@gmail.com>
2022-06-21 16:18:09 -0700

    Corrections

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[counter]), end="")
             characters += 1
-        except:
+        except Exception:
             break
     print()
     return characters
